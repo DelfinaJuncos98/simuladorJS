7e25982ef69bc5cb14e93d037b5e6e4da228ce53
Delfina <dgiuliodori@uade.edu.ar>
2023-07-17 00:46:48 -0300

    feature/primera-entrega-simulador: segunda pre-entrega

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,3 +1,18 @@
+function persona (nombre1, apellido1, correo1, usuario1, contra1){
+    this.nombre = nombre1;
+    this.apellido = apellido1;
+    this.correo = correo1;
+    this.usuario = usuario1;
+    this.contra = contra1;
+
+}
+const ClientePreArmado = new persona("John", "doe", "johndoe@gmail.com", "johndoe1", "contraseña")
+const ClientePreArmado1 = new persona("Lucy", "doe", "lucydoe@gmail.com", "lucydoe1", "contraseña")
+const ClientePreArmado2 = new persona("Lucas", "doe", "lucasoe@gmail.com", "lucasdoe1", "contraseña")
+const listaClientes= []
+
+const ListaClientesPreArmada = [ClientePreArmado.usuario, ClientePreArmado1.usuario, ClientePreArmado2.usuario, ClientePreArmado.contra, ClientePreArmado1.contra, ClientePreArmado2.contra]
+
 function login() {
     alert("Bienvenido Happy Kids!")
     let cliente = prompt("Es usted cliente de Happy Kids ? \nEn caso de ser cliente ingrese SI de lo contrario ingrese NO");
@@ -17,25 +32,76 @@ function login() {
         while (correo == ""){
             correo= prompt("Ingrese un correo electronico valido")
         }
-         usuario= prompt("Ingrese un usuario")
+         let usuario = prompt("Ingrese un usuario")
         while (usuario == ""){
             usuario= prompt("Ingrese un usuario valido")
         }
-         contra = prompt("Ingrese una contraseña, esta debe ser de al menos 8 caracteres ")
+         let contra = prompt("Ingrese una contraseña, esta debe ser de al menos 8 caracteres ")
         while (contra.length <= 7 ){
             contra = prompt("introduzca una contraseña valida")
         }
-        alert("Bienveido" + usuario + "! \n Esperamos que tengas una experiencia increible" )
-    }else{
-        let usuario = prompt("ingrese su usuario")
-        while (usuario.length == 0 ){
-            usuario = prompt("ingrese un usuario valido")
-        }
-        let contra = prompt("ingrse su contraseña")
-        while (contra.length == 0 ){
-            contra = prompt("ingrse un contraseña valida")
-        }
-        alert("¡Hola " + usuario +"! \n ¡Te extrañamos!") 
+        alert("Bienveido " + usuario + " ! \n Esperamos que tengas una experiencia increible" )
+
+        const nuevoCliente = new persona(nombre,apellido,correo,usuario,contra)
+        alert("Su usuario se creo correctamente con los siguientes datos:" 
+        + "\n Nombre: " + nuevoCliente.nombre 
+        + "\n Apellido: " + nuevoCliente.apellido
+        + "\n Correo Electronico : " + nuevoCliente.correo
+        + "\n Usuario: " + nuevoCliente.usuario)
+
+        listaClientes.push(nuevoCliente.usuario)
+        listaClientes.push(nuevoCliente.contra)
+
+    let logIn= prompt("¿Desea loguearse? Si, la respuesta es afirmativa ingrese SI de lo contrario NO ");
+    while (logIn != "si" && cliente != "no"){
+        logIn = prompt("Por favor ingrese una resuesta valida. \n Desea loguearse? Si, la respuesta es afirmativa ingrese SI de lo contrario NO");
     }
+
+    if (logIn == "si"){
+        let usuario= prompt("ingrese su Usuario")
+        while (listaClientes.includes(usuario) == false){
+            usuario= prompt("ingrese un nombre Usuario valido")
+        } 
+        let contra= prompt("ingrese su contraseña")
+        while (listaClientes.includes(contra) == false){
+            usuario= prompt("ingrese una contraseña valida")
+        } 
+        alert("¡Hola " + usuario +"! ") 
+        eliminarCuenta = prompt("¿Desea eliminar su cuenta? si, la respuesta es afirmativa ingrese SI de lo contrario NO ")
+        if (eliminarCuenta == "si"){
+            let usuarioEliminar= prompt("ingrese su Usuario")
+            let usuario2= usuarioEliminar
+            while (listaClientes.includes(usuarioEliminar) == false){
+                usuario = prompt("ingrese un nombre Usuario valido")
+            } 
+            listaClientes.pop(usuarioEliminar)
+            console.log(listaClientes)
+            alert("Hasta la proxima " + usuario2 + "!")
+        } 
+        }else{
+        alert("Hasta la proxima!") 
+    }
+
+    }else{
+        
+        alert("En el inspect va a encontar una lista de usuarios pre-armada con la que puede probar este Log In")
+        console.log("Usuario: " + ClientePreArmado.usuario  + "\n Contraseña: " + ClientePreArmado.contra);
+        console.log("Usuario: " + ClientePreArmado1.usuario  + "\n Contraseña: " + ClientePreArmado1.contra);
+        console.log("Usuario: " + ClientePreArmado2.usuario  + "\n Contraseña: " + ClientePreArmado2.contra);
+
+        let usuarioPreArmado = prompt("ingrese su usuario")
+        while (ListaClientesPreArmada.includes(usuarioPreArmado) == false){
+            console.log(usuarioPreArmado, ListaClientesPreArmada)
+            usuarioPreArmado = prompt("ingrese un usuario valido")
+        }
+        let contraPreArmada = prompt("ingrese su contraseña")
+        while (ListaClientesPreArmada.includes(contraPreArmada) == false ){
+            contraPreArmada = prompt("ingrese una contraseña valida")
+        }
+        alert("Bienvenido " + usuarioPreArmado + "!")
+    }
+
+
+    
 }
 login();
